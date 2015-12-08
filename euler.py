64acccac2555f9bdebacc0eb4590117ebd94da47
Deja <dawid.deja@intel.com>
2015-12-08 21:52:20 +0100

    Wyliczanie rozwiazania metoda Eulera

--- euler.py
+++ euler.py
@@ -1,11 +1,28 @@
 def function(x, t):
-    return x*t
-    
+    return x
+
+
+def arange(start, stop, step):
+    """since range only accepts integers
+    we must use our own function"""
+    r = start
+    while r <= stop:
+        yield r
+        r += step
+
+
 def main():
-    x0 = float(input("Podaj x0: "))
+    x = float(input("Podaj x0: "))
     eps = float(input("Podaj dokladnosc: "))
-    
-    print(function(x0, eps))
-    
+
+    h = 1  # step
+    points = [(0,x)]
+
+    for i in arange(h, 4, h):
+        x = x + h * function(x,i)
+        points.append((i,x))
+
+    print(points)
+
 if __name__ == '__main__':
     main()
