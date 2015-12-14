872e2b34b16c62c0249e66818e836a90d27d6c7f
Dawid Deja <dejson@Dejson-Netbook.(none)>
2015-12-14 20:21:55 +0100

    Wyliczanie kroku na podstawie bledu globalnego

--- euler.py
+++ euler.py
@@ -1,8 +1,13 @@
 import matplotlib.pyplot as plt
+import numpy
+
+
+M=1.0
+L=1.0
 
 
 def function(x, t):
-    return x
+    return numpy.cos(x*t*1000)
 
 
 def arange(start, stop, step):
@@ -14,14 +19,27 @@ def arange(start, stop, step):
         r += step
 
 
+def GTE(h):
+    """Calculate global truncation error"""
+    return h*M/(2*L) * (numpy.e**L - 1)
+
+
+def calculate_step(h, eps):
+    if GTE(h) < eps:
+        return h
+
+    return calculate_step(h/2, eps)
+
+
 def main():
     x = float(input("Podaj x0: "))
     eps = float(input("Podaj dokladnosc: "))
 
-    h = 1  # step
+    h = calculate_step(1.0, eps)  # step
+    print("Krok: %f" % h)
     points = [(0,x)]
 
-    for i in arange(h, 4, h):
+    for i in arange(h, 1, h):
         x = x + h * function(x,i)
         points.append((i,x))
 
